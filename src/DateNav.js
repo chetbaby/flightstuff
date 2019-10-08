dc0ea7c3c0428b4f92d9fdc6aa0a9decc058a7a0
chetbaby <chet.hay@gmail.com>
2019-10-07 22:54:43 -0700

    header nav

--- src/DateNav.js
+++ src/DateNav.js
@@ -1,7 +1,15 @@
 import React from 'react';
+import moment from 'moment';
+import H2Title from './H2Title';
 
-function DateNav() {
-  return <div>a</div>;
-}
+const DateNav = () => (
+  <section className="header-nav">
+    <button className="button" type="button">
+      prev
+    </button>
+    <H2Title title={moment().format('MMMM Do YYYY')} />
+    <button type="button">next</button>
+  </section>
+);
 
 export default DateNav;
---
+++ src/H2Title.js
@@ -0,0 +1,6 @@
+import React from 'react';
+
+const H2Title = ({ title, align }) => (
+  <h2 className={`h2 ${align}`}>{title}</h2>
+);
+export default H2Title;
